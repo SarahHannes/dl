5618d2fc8cbe13f0a9a2ee690781ba1b602b5c1b
Sarah Hannes <78901374+SarahHannes@users.noreply.github.com>
2022-04-05 17:29:57 +0800

    Load model to streamlit app

--- cnn/20_bread_app.py
+++ cnn/20_bread_app.py
@@ -5,26 +5,27 @@ Created on Tue Apr  5 15:19:19 2022
 @author: Sarah
 """
 
-import requests
 import os
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-import h5py
-
-# Download best model
 import urllib.request
 
-urllib.request.urlretrieve(
-        'https://github.com/SarahHannes/dl/raw/e1a0ee81c43f69772842187f980694a29b8d19cc/cnn/model/20_bread_weights-improvement-09-0.88.hdf5', 'model.hdf5')
-MODEL_PATH = './model.hdf5'
+# Download best model
+@st.cache(allow_output_mutation=True)
+def load_model():
+        urllib.request.urlretrieve(
+                # Source url: using github permalink
+                'https://github.com/SarahHannes/dl/raw/e1a0ee81c43f69772842187f980694a29b8d19cc/cnn/model/20_bread_weights-improvement-09-0.88.hdf5',
+                # Destination path
+                'model.hdf5')
+        MODEL_PATH = './model.hdf5'
+        model = tf.keras.models.load_model(MODEL_PATH)
+        return model
 
-# model_filename = "20_bread_weights-improvement-09-0.88.hdf5"
-# url = "https://github.com/SarahHannes/dl/raw/e1a0ee81c43f69772842187f980694a29b8d19cc/cnn/model/" + model_filename
-# r = requests.get(url)
-# model_path = open(model_filename , 'wb').write(r.content)
-model = tf.keras.models.load_model(MODEL_PATH)
-st.write(model.summary())
+# Upload image
+# Predict image
+# Plot image
 
 st.write('Good Bread Moldy Bread classifier 🍞🥐🥖')
 img = st.file_uploader("Upload your bread image!")
