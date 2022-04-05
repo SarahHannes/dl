c990e4b36855b3a5b93502026f6f71cd086abd2d
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2022-04-05 18:19:00 +0800

    Predict user uploaded image

--- cnn/20_bread_app.py
+++ cnn/20_bread_app.py
@@ -1,35 +1,52 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr  5 15:19:19 2022
+import os
+import urllib.request
 
-@author: Sarah
-"""
-
-import streamlit as st
-import cv2
-import imutils
+from PIL import Image, ImageOps
 import numpy as np
-import wget
+import streamlit as st
 import tensorflow as tf
 
-# Download best model
-download_from = "https://github.com/SarahHannes/dl/raw/main/cnn/model/20_bread_weights-improvement-09-0.88.hdf5"
-download_to = "model.hdf5"
-model_path = wget.download(download_from, out = download_to)
-model = tf.keras.models.load_model(model_path)
 
-st.write('Good Bread Moldy Bread classifier 🍞🥐🥖')
-img = st.file_uploader("Upload your bread image!")
-st.image(img)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0) # Create a batch
+@st.cache(allow_output_mutation=True)
+def load_model():
+    """
+    Load saved model from source url.
+    :return: keras.engine.sequential.Sequential
+    """
+    urllib.request.urlretrieve(
+        # Source url: using gitHub permalink
+        'https://github.com/SarahHannes/dl/raw/e1a0ee81c43f69772842187f980694a29b8d19cc/cnn/model/20_bread_weights-improvement-09-0.88.hdf5',
+        # Destination path
+        'model.hdf5')
+    model_path = './model.hdf5'
+    model = tf.keras.models.load_model(model_path)
+    return model
 
-# Predict using loaded model
-predictions = model3.predict(img_array)
-score = tf.nn.softmax(predictions[0])
+def get_prediction(image):
+    """
+    Get class label and confidence level for user uploaded image.
+    :param image: Uploaded image file
+    :return: str, float
+    """
+    class_names = ['good', 'moldy']
+    model = load_model()
+    predictions = model.predict(image)
+    score = tf.nn.softmax(predictions[0])
+    predicted_class = class_names[np.argmax(score)
+    return predicted_class, 100 * np.max(score)
 
-# Plot user input image
-plt.imshow(img_array[0].numpy().astype("uint8"))
-plt.title("Predicted class: {} ({:.2f}% confidence)".format(class_names[np.argmax(score)], 100 * np.max(score)))
-plt.axis('off')
-plt.show()
+
+st.write('To eat or not to eat... 🍞🥐🥖')
+uploaded_file = st.file_uploader("Upload your bread image!", type='jpg')
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded image", use_column_width=True)
+    st.write("")
+    prediction = get_prediction(image)
+    st.write(prediction[0], f"\nConfidence level: .2f{prediction[1]}")
+
+
+# st.image(image)
+# img_array = tf.keras.utils.img_to_array(image)
+# img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
